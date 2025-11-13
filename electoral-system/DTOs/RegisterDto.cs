669dcf17fa878f6fd8ecf3694186dbc155167c1a
Dany Paz <danysantiago2505@gmail.com>
2025-11-12 20:40:17 -0600

    agregando Registro

---
+++ electoral-system/DTOs/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace electoral_system.DTOs;
+
+public class RegisterDto
+{
+    [Required(ErrorMessage = "El email es requerido")]
+    [EmailAddress(ErrorMessage = "El  email es invalido")]
+    public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "El password es requerido")]
+    [MinLength(6, ErrorMessage = "El password debe tener 6 caracteres")]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "El username es requerido")]
+    public string FullName { get; set; } = string.Empty;
+    
+}
